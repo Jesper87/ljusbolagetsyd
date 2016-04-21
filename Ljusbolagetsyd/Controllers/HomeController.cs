c6781272e935c4fdc556929508dc1a445cc75e0b
Jesper <jesperdahlberg400@hotmail.com>
2016-04-21 21:06:39 +0200

    Gallery

--- Ljusbolagetsyd/Controllers/HomeController.cs
+++ Ljusbolagetsyd/Controllers/HomeController.cs
@@ -5,63 +5,62 @@ using Ljusbolagetsyd.Models;
 
 namespace Ljusbolagetsyd.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
-    {
-      return View();
-    }
-
-    public ActionResult About()
-    {
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      return View("Contact", new ContactFormViewModel());
-    }
-
-    public ActionResult Gallery()
-    {
-
-      var path3 = Server.MapPath("~/Content/Images/Gallery");
-
-      var service = new ImageService();
-      var images = service.GetImagesFromContentFolder(path3);
-      foreach (var galleryImage in images)
+   public class HomeController : Controller
+   {
+      public ActionResult Index()
       {
-        var str = galleryImage.ImageUrl.Split('\\');
-        var last = str[str.Length-1];
-        galleryImage.ImageUrl = "/Content/Images/Gallery/" + last;
+         return View();
       }
 
-      return View(images);
-    }
-
-    public ActionResult Equipment()
-    {
-      return View();
-    }
-
-    public ActionResult Services()
-    {
-      return View();
-    }
-
-    [ValidateAntiForgeryToken]
-    [HttpPost]
-    public ActionResult ContactForm(ContactFormViewModel model)
-    {
-      if (ModelState.IsValid)
+      public ActionResult About()
       {
-        var isSent = EmailService.SendNewEmail(model);
-        ModelState.Clear();
-        return View("Contact", new ContactFormViewModel { Succes = isSent });
-
+         return View();
       }
-      return View("Contact");
-    }
 
-  }
+      public ActionResult Contact()
+      {
+         return View("Contact", new ContactFormViewModel());
+      }
+
+      public ActionResult Gallery()
+      {
+
+         var path3 = Server.MapPath("~/Content/Images/Gallery");
+
+         var service = new ImageService();
+         var images = service.GetImagesFromContentFolder(path3);
+         foreach (var galleryImage in images)
+         {
+            var str = galleryImage.ImageUrl.Split('\\');
+            var last = str[str.Length - 1];
+            galleryImage.ImageUrl = "/Content/Images/Gallery/" + last;
+         }
+         return View(images);
+      }
+
+      public ActionResult Equipment()
+      {
+         return View();
+      }
+
+      public ActionResult Services()
+      {
+         return View();
+      }
+
+      [ValidateAntiForgeryToken]
+      [HttpPost]
+      public ActionResult ContactForm(ContactFormViewModel model)
+      {
+         if (ModelState.IsValid)
+         {
+            var isSent = EmailService.SendNewEmail(model);
+            ModelState.Clear();
+            return View("Contact", new ContactFormViewModel { Succes = isSent });
+
+         }
+         return View("Contact");
+      }
+
+   }
 }
